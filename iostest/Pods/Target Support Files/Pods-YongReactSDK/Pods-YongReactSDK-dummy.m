2c9b7c9ae0cac76a52b7b4d3eeb7e63b2fe46946
didibao <didibao@163.com>
2019-09-25 19:28:23 +1000

    rename ios folder

--- iostest/Pods/Target Support Files/Pods-YongReactSDK/Pods-YongReactSDK-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_YongReactSDK : NSObject
-@end
-@implementation PodsDummy_Pods_YongReactSDK
-@end
--- iostest/Pods/Target Support Files/Pods-YongReactSDKTests/Pods-YongReactSDKTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_YongReactSDKTests : NSObject
-@end
-@implementation PodsDummy_Pods_YongReactSDKTests
-@end
