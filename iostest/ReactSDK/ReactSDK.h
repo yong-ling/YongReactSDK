b8be3ccdd59357a93682f97bf9917c007e99fa7d
didibao <didibao@163.com>
2019-09-25 19:26:51 +1000

    ios

---
+++ iostest/ReactSDK/ReactSDK.h
@@ -0,0 +1,19 @@
+//
+//  ReactSDK.h
+//  ReactSDK
+//
+//  Created by johnnyddb on 25/9/19.
+//  Copyright © 2019 Facebook. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ReactSDK.
+FOUNDATION_EXPORT double ReactSDKVersionNumber;
+
+//! Project version string for ReactSDK.
+FOUNDATION_EXPORT const unsigned char ReactSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ReactSDK/PublicHeader.h>
+
+
